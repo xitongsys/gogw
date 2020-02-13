0c1da196e75a3d09889a63f986c5dc7511924fd6
xitongsys <xitongsys@gmail.com>
2020-02-13 22:15:56 +0800

    uuid test

--- common/util.go
+++ common/util.go
@@ -1,9 +1,18 @@
 package common
 
 import (
+	"fmt"
+	
 	"github.com/google/uuid"
 )
 
+var ID = 0
+
 func UUID() string {
+	ID++
+	return fmt.Sprint(ID)
+}
+
+func UUID0() string {
 	return uuid.New().String()
 }
